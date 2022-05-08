b89017315d32284d45e05e0fcf4396c2cb063d89
f1rpo <no.m@il.specified>
2022-05-09 01:24:07 +0200

    Comment change; no longer exposing anything to Python.
    Amends commit
    639f6d8d41c9b43e6442e2ab0e1c80230394fe98

--- CvGameCoreDLL/SelfMod.h
+++ CvGameCoreDLL/SelfMod.h
@@ -3,14 +3,13 @@
 #define SELF_MOD_H
 
 /*	smcpoc: Let's put all runtime patches for Civ4BeyondSword.exe (v3.19)
-	in a single place. (But, so far, there's only one.) For the time being,
-	functions are exposed to Python through CyGameCoreUtils. */
+	in a single place. (But, so far, there's only one.) */
 
 class Civ4BeyondSwordMods
 {
 public:
 	bool isPlotIndicatorSizePatched() const { return m_bPlotIndicatorSizePatched; }
-	void patchPlotIndicatorSize(); // (exposed to Python)
+	void patchPlotIndicatorSize();
 private:
 	bool m_bPlotIndicatorSizePatched;
 };
